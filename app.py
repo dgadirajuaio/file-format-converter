9a65c1830dbd7341488f8016ebe6eb731f3c1118
dgadirajuaio <durga.gadiraju@analytiqs.io>
2023-03-12 17:20:23 +0000

    Refactor core conversion logic using process_file function

--- app.py
+++ app.py
@@ -8,10 +8,18 @@ import pandas as pd
 # Folder paths are hard coded
 # schemas.json path is also hard coded
 # Modularization with reusability
+
 # Exercise: Similar program file for nyse-converter
   # Update app.py with core logic of file format conversion for nyse data
   # Make sure to validate to see if it is working as per the expectations or not
   # Changes are committed and pushed to remote repository
+
+# * Exercise: Make changes to nyse-converter to use environment variables.
+#   * SRC_DIR: `data/nyse_all/nyse_data`
+#   * TGT_DIR: `data/nyse_all/nyse_json`
+#   * Make sure to run and validate the code after making changes using environment variables.
+#   * Make sure to commit and push the changes to remote git repository.
+
 def get_columns(ds):
     schemas_file_path = os.environ.setdefault('SCHEMAS_FILE_PATH', 'data/retail_db/schemas.json')
     with open(schemas_file_path) as fp:
@@ -28,21 +36,24 @@ def get_columns(ds):
         return
 
 
+def process_file(src_base_dir, ds, tgt_base_dir):
+    for file in glob.glob(f'{src_base_dir}/{ds}/part*'):
+        df = pd.read_csv(file, names=get_columns(ds))
+        os.makedirs(f'{tgt_base_dir}/{ds}', exist_ok=True)
+        df.to_json(
+            f'{tgt_base_dir}/{ds}/part-{str(uuid.uuid1())}.json',
+            orient='records',
+            lines=True
+        )
+        print(f'Number of records processed for {os.path.split(file)[1]} in {ds} is {df.shape[0]}')
+
+
 def main():
     src_base_dir = os.environ['SRC_BASE_DIR']
     tgt_base_dir = os.environ['TGT_BASE_DIR']
     for path in glob.glob(f'{src_base_dir}/*'):
         if os.path.isdir(path):
-            ds = os.path.split(path)[1]
-            for file in glob.glob(f'{path}/part*'):
-                df = pd.read_csv(file, names=get_columns(ds))
-                os.makedirs(f'{tgt_base_dir}/{ds}', exist_ok=True)
-                df.to_json(
-                    f'{tgt_base_dir}/{ds}/part-{str(uuid.uuid1())}.json',
-                    orient='records',
-                    lines=True
-                )
-                print(f'Number of records processed for {os.path.split(file)[1]} in {ds} is {df.shape[0]}')
+            process_file(src_base_dir, os.path.split(path)[1], tgt_base_dir)
 
 
 if __name__ == '__main__':

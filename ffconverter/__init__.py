bfa6ac463abb5558d6f50faf0dac595d5d8e6f5a
dgadirajuaio <durga.gadiraju@analytiqs.io>
2023-03-15 10:26:40 +0000

    Added logic to convert application to executable command

--- ffconverter/__init__.py
+++ ffconverter/__init__.py
@@ -53,14 +53,15 @@ def process_file(src_base_dir, ds, tgt_base_dir):
 
 
 def main():
+    log_file_path = os.environ['LOG_FILE_PATH']
+    src_base_dir = os.environ['SRC_BASE_DIR']
+    tgt_base_dir = os.environ['TGT_BASE_DIR']
     logging.basicConfig(
-        filename='logs/ffc.log',
+        filename=log_file_path,
         level=logging.INFO, 
         format='%(levelname)s %(asctime)s %(message)s',
         datefmt='%Y-%m-%d %I:%M:%S %p'
     )
-    src_base_dir = os.environ['SRC_BASE_DIR']
-    tgt_base_dir = os.environ['TGT_BASE_DIR']
     datasets = os.environ.get('DATASETS') # orders,order_items
     logging.info('File format conversion: Started')
     if not datasets:
